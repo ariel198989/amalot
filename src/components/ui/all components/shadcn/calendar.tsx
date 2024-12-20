a1e9bee53f697b8a9c2da3f38bec8224aa82b6bc
Ariel198989 <ariel@ao-fin.co.il>
2024-12-20 10:23:12 +0200

    hjk

--- src/components/ui/all components/shadcn/calendar.tsx
+++ src/components/ui/all components/shadcn/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        NextIcon: () => <ChevronRight className="h-4 w-4" />,
-        PrevIcon: () => <ChevronLeft className="h-4 w-4" />
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />
       }}
       {...props}
     />
